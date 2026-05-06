629c7d8f6080dc7e17c9dfe2f73e7103d20d971b
amritagoswami26 <amritagoswami198@gmail.com>
2026-05-06 14:13:54 +0530

    Add files via upload

---
+++ TicTacToeUC10.java
@@ -0,0 +1,19 @@
+public class TicTacToeUC10{
+
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        System.out.println(isDraw());
+    }
+
+    static boolean isDraw() {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
